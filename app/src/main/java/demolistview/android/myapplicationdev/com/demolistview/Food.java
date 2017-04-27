7762eeed9210bad272ec9c3c9043801cdb419b9c
15004557 <H0peyur3gd7>
2017-04-27 10:52:52 +0800

    ListView Example

---
+++ app/src/main/java/demolistview/android/myapplicationdev/com/demolistview/Food.java
@@ -0,0 +1,20 @@
+package demolistview.android.myapplicationdev.com.demolistview;
+
+/**
+ * Created by 15004557 on 27/4/2017.
+ */
+
+public class Food {
+    private String name;
+    private boolean star;
+    public Food(String name, boolean star) {
+        this.name = name;
+        this.star = star;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isStar() {
+        return star;
+    }
+}
